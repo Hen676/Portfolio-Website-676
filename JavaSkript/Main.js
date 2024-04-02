16c7976fe4ab1879324e231c6ab7b5b2a7a62981
Henry Timms <36480544+Hen676@users.noreply.github.com>
2024-04-02 23:00:42 +0100

    Minor code clean up
    Improve image popup
    Fixed scroll to top button on first load
    Reduced flash bang effect on first load when in dark mode

--- JavaSkript/Main.js
+++ JavaSkript/Main.js
@@ -1,107 +1,108 @@
-// Global vars
-var body = document.getElementById("body");
-
 var modal = document.getElementById("modal");
 var modalImg = document.getElementById("imgModal");
 var modalClose = document.getElementById("closeModal");
 var captionText = document.getElementById("captionModal");
 
-var topArrow = document.getElementById("topArrow");
-var imgSect = document.getElementsByClassName("imageSection");
-
-var darkMode = document.getElementById("darkMode");
-var darkModeIcon = document.getElementById("darkModeIcon");
-var darkModeText = document.getElementById("darkModeText");
-
 var isMobile = mobileCheck();
 
-// Loop image sections
-var i;
-var j;
-for (i = 0; i < imgSect.length; i++) {
-  // Loop image section children
-  var div = imgSect[i].children;
-  for (j = 0; j < div.length; j++) {
-    div[j].children[0].onclick = function () {
-      modal.style.display = "flex";
-      body.style.overflow = "hidden";
-      modalImg.src = this.src;
-      if (this.alt != "") {
-        captionText.style.display = "block";
-        captionText.innerHTML = this.alt;
-      } else captionText.style.display = "none";
+function load() {
+	var body = document.getElementById("body");
 
-      if (isMobile) body.style.padding = "0";
-      else body.style.padding = "0 17px 0 0";
-    };
-  }
+	// Dark mode
+	var isStorageAvailable = typeof Storage !== "undefined";
+	if (isStorageAvailable) {
+		if (localStorage.getItem("dark") === "true") {
+			body.classList.add("dark");
+			toggleDarkModeIcon();
+		}
+	}
+	var darkMode = document.getElementById("darkMode");
+	darkMode.onclick = function () {
+		body.classList.toggle("dark");
+
+		var darkModeBool = body.classList.contains("dark");
+		toggleDarkModeIcon(darkModeBool);
+
+		if (isStorageAvailable) localStorage.setItem("dark", darkModeBool);
+	};
+
+	// Image sections
+	var modal = document.getElementById("modal");
+	var modalImg = document.getElementById("imgModal");
+	var modalClose = document.getElementById("closeModal");
+	var captionText = document.getElementById("captionModal");
+	for (let element of document.getElementsByClassName("imageSection")) {
+		for (let div of element.children) {
+			div.children[0].onclick = function () {
+				modal.style.display = "flex";
+				body.style.overflow = "hidden";
+				modalImg.src = this.src;
+				if (this.alt != "") {
+					captionText.style.display = "block";
+					captionText.innerHTML = this.alt;
+				} else captionText.style.display = "none";
+
+				if (isMobile) body.style.padding = "0";
+				else body.style.padding = "0 17px 0 0";
+			};
+		}
+	}
+	modalClose.onclick = close;
+	window.onkeydown = function (event) {
+		if (event.keyCode == 27) close();
+	};
+
+	var topArrow = document.getElementById("topArrow");
+	// Scroll to top arrow
+	window.onscroll = function () {
+		if (isMobile) {
+			topArrow.style.display = "none";
+			return;
+		}
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+			topArrow.style.display = "flex";
+		else topArrow.style.display = "none";
+	};
+	topArrow.onclick = function () {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	};
+
+	body.style.background = "";
 }
 
 // Close Popup via span and ESC
 function close() {
-  body.style.overflow = "auto";
-  modal.style.display = "none";
-  body.style.padding = "0";
+	body.style.overflow = "auto";
+	modal.style.display = "none";
+	body.style.padding = "0";
 }
-modalClose.onclick = close;
-window.onkeydown = function (event) {
-  if (event.keyCode == 27) close();
-};
 
-// Dark mode
-var isStorageAvailable = typeof Storage !== "undefined";
-if (isStorageAvailable) {
-  if (localStorage.getItem("dark") === "true") {
-    body.classList.add("dark");
-    toggleDarkModeIcon();
-  }
-}
-darkMode.onclick = function () {
-  body.classList.toggle("dark");
-
-  var darkModeBool = body.classList.contains("dark");
-  toggleDarkModeIcon(darkModeBool);
-
-  if (isStorageAvailable)
-    localStorage.setItem("dark", darkModeBool);
-};
 function toggleDarkModeIcon(toggle = true) {
-  darkModeIcon.classList.toggle("fa-moon");
-  darkModeIcon.classList.toggle("fa-sun");
-  if (toggle)
-    darkModeText.textContent = "Light Mode"
-  else
-    darkModeText.textContent = "Dark Mode"
-}
+	var darkModeIcon = document.getElementById("darkModeIcon");
+	var darkModeText = document.getElementById("darkModeText");
 
-// Scroll to top arrow
-window.onscroll = function () {
-  if (isMobile) {
-    topArrow.style.display = "none";
-    return;
-  }
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
-    topArrow.style.display = "flex";
-  else topArrow.style.display = "none";
-};
-topArrow.onclick = function () {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
+	darkModeIcon.classList.toggle("fa-moon");
+	darkModeIcon.classList.toggle("fa-sun");
+	if (toggle) darkModeText.textContent = "Light Mode";
+	else darkModeText.textContent = "Dark Mode";
+}
 
 // Util
 function mobileCheck() {
-  let check = false;
-  (function (a) {
-    if (
-      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-        a
-      ) ||
-      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-        a.substr(0, 4)
-      )
-    )
-      check = true;
-  })(navigator.userAgent || navigator.vendor || window.opera);
-  return check;
+	let check = false;
+	(function (a) {
+		if (
+			/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+				a
+			) ||
+			/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+				a.substr(0, 4)
+			)
+		)
+			check = true;
+	})(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
 }
+
+window.onload = load();
